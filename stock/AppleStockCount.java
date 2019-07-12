66abdfd427cd19b9848f26a1d1135019f49a664a
coder <nompetsheniayabonga@gmail.com>
2019-07-12 12:01:54 +0200

    first commit

---
+++ stock/AppleStockCount.java
@@ -0,0 +1,38 @@
+package stock;
+public class AppleStockCount{
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Please give me a qty!");
+            return;
+        }
+    
+        int qty = Integer.parseInt(args[0]);
+        double cost = 4.50;
+        double total;
+    
+        if (qty < 1) {
+            System.out.println("Out of stock.");
+        }
+        else {
+          total = qty * cost;
+    
+          StringBuilder message = new StringBuilder();
+          message.append("You have bought ");
+          message.append(qty);
+          message.append(" apple");
+          if (qty > 1) {
+            message.append("s");
+          }
+          message.append(" for a total cost of ");
+          message.append(total);
+          message.append(".");
+    
+          System.out.println(message.toString());
+    
+        }
+    
+    
+      }
+};
