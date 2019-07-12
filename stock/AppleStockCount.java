88c139947c49261bb3393bfff208be82a3ee50c6
coder <nompetsheniayabonga@gmail.com>
2019-07-12 15:47:33 +0200

    added if statements for my applestock counter

--- stock/AppleStockCount.java
+++ stock/AppleStockCount.java
@@ -11,13 +11,31 @@ public class AppleStockCount{
         int qty = Integer.parseInt(args[0]);
         double cost = 4.50;
         double total;
+        int dozen = 0;
     
         if (qty < 1) {
             System.out.println("Out of stock.");
         }
-        else {
+        else if (qty / 12 >0 ){
+
           total = qty * cost;
-    
+         if (qty >= 12 ){
+           dozen = qty/12;
+
+           StringBuilder message = new StringBuilder();
+           message.append("You have bought ");
+           message.append(dozen);
+           message.append(" dozen");
+           if (qty > 1) {
+             message.append("s");
+           }
+           message.append(" for a total cost of ");
+           message.append(total);
+           message.append(".");
+     
+           System.out.println(message.toString());
+
+         }else{
           StringBuilder message = new StringBuilder();
           message.append("You have bought ");
           message.append(qty);
@@ -30,9 +48,8 @@ public class AppleStockCount{
           message.append(".");
     
           System.out.println(message.toString());
-    
+        
         }
-    
-    
       }
+    }
 };
